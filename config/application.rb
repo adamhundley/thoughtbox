65316fbd5f63c31deb82c59ffbc85f9585985523
Adam Hundley <adamhundley@gmail.com>
2016-06-06 21:50:51 -0600

    trying to fix deploy bug

--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,5 @@
 require File.expand_path('../boot', __FILE__)
 
-config.assets.initialize_on_precompile = false
 
 require "rails"
 # Pick the frameworks you want:
@@ -19,6 +18,7 @@ Bundler.require(*Rails.groups)
 
 module Thoughtbox
   class Application < Rails::Application
+    config.assets.initialize_on_precompile = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
